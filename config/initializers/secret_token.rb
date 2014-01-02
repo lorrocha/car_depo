04b6b6200b5739b0ffe3441f8ae98cacb46867f4
Lorry Rocha <sorai120@yahoo.com>
2014-01-01 22:38:17 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CarDepo::Application.config.secret_key_base = 'c7f7504f9f2c3e717c81d128460478d391ea0bcb4b99c63419fa7dafd2e6c29aa8d69ece5a0c86144e4024c2e4f56930c512ee39664f1e4c3e64cd3684dffde7'
